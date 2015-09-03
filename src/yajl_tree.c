2c42d2a07e68bfdfb1d16c0ed7efd31f327aae49
Henry Stern <stern@fsi.io>
2015-09-03 11:26:37 -0300

    yajl_tree_parse: fix memory leak on error
    ctx.stack was not being cleaned up after a parse error. this patch pops
    all remaining elements and frees them.

--- src/yajl_tree.c
+++ src/yajl_tree.c
@@ -458,6 +458,9 @@ yajl_val yajl_tree_parse_options (const char *input,
              snprintf(error_buffer, error_buffer_size, "%s", internal_err_str);
              YA_FREE(&(handle->alloc), internal_err_str);
         }
+        while (ctx.stack) {
+            yajl_tree_free(context_pop(&ctx));
+        }
         yajl_free (handle);
         return NULL;
     }
